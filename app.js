ea9ac9f916112d3a40e6cb094cbd3c8566d6cb20
bkoimett <benkoimet@gmail.com>
2025-09-18 07:04:04 +0300

    feat: implement blog application structure with routes, controllers, and models

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
+
+// express app initialisation
+const app = express();
+
+// connect to MongoDB
+const dbURI =
+  "mongodb+srv://koimettb:1738@nodejstuts.rmg3aqa.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=nodejstuts";
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000) && console.log("connection successful !")) // only listen if connection is made
+  .catch((err) => console.log(err));
+
+// register view engine
+app.set("view engine", "ejs");
+
+// middleware and static - for all req including post
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+// route handlers
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
+});
+
+// blog routes
+app.use("/blogs", blogRoutes); // since we have scoped out the blogs, it makes the routes reusable
+
+// 404 page - always at the bottom
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
---
+++ routes/blogRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const blogController = require("../controllers/blogController");
+const router = express.Router();
+
+// create route
+// router.get("/create", blogController.blog_create_get);
+
+// index - find blog
+router.get("/", blogController.blog_index);
+
+// post route handler
+router.post("/", blogController.blog_create_post);
+
+// findbyId route
+router.get("/:id", blogController.blog_details);
+
+// delete blog route
+router.delete("/:id", blogController.blog_delete);
+
+module.exports = router;
